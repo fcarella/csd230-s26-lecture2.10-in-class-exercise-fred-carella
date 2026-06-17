3c1962bc163e7a32b957c605b1a6a183c83d805d
fred carella <fred.carela@gmailcom>
2026-06-17 14:11:09 -0400

    commit

--- src/main/java/app/Application.java
+++ src/main/java/app/Application.java
@@ -1,11 +1,13 @@
 package app;
 
+import app.config.RsaKeyProperties;
 import com.github.javafaker.Faker;
 import app.entities.*;
 import app.repositories.*;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -15,6 +17,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import java.util.List;
 
 @SpringBootApplication
+@EnableConfigurationProperties(RsaKeyProperties.class)
 public class Application {
 
     public static void main(String[] args) {
